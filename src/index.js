c1773d035b44011a9545089f2664ca5bcf85e412
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-04-17 22:44:01 -0300

    firebase auth, and firestore

---
+++ src/index.js
@@ -0,0 +1,118 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  orderBy,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOeAoxuRK0LCcOeVtYq5XHy5ePdLybV1A",
+  authDomain: "fir-9-dojo-986f7.firebaseapp.com",
+  projectId: "fir-9-dojo-986f7",
+  storageBucket: "fir-9-dojo-986f7.appspot.com",
+  messaginSenderId: "310675130670",
+  appId: "1:310675130670:web:6e49d7dbb0da7701ca12bf",
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+const auth = getAuth();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// queries
+const q = query(
+  colRef,
+  // where("author", "==", "patrick rothfuss"),
+  orderBy("createdAt")
+);
+
+// realtime collection data
+onSnapshot(q, snapshot => {
+  let books = [];
+  snapshot.docs.forEach(doc => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+});
+
+// adding docs
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+    createdAt: serverTimestamp(),
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
+
+// deleting docs
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", deleteBookForm.id.value);
+
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
+});
+
+// get a single document
+
+const docRef = doc(db, "books", "XWzsa9aW74mGFh8InLuU");
+
+onSnapshot(docRef, doc => {
+  console.log(doc.data(), doc.id);
+});
+
+// updating a document
+
+const updateForm = document.querySelector(".update");
+updateForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", updateForm.id.value);
+  updateDoc(docRef, {
+    title: "The Name of the Wind",
+  }).then(() => {
+    updateForm.reset();
+  });
+});
+
+// signing users up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log(`user created: ${cred.user}`);
+      signupForm.reset();
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
